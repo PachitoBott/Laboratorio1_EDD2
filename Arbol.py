55b94eaa0a05d09f7ed226976da4f46a4e105556
Santiago <sahiggins@uninorte.edu.co>
2026-03-18 21:52:00 -0500

    añadido delete y find

--- Arbol.py
+++ Arbol.py
@@ -60,4 +60,44 @@ class Arbol:
                 current_node.right = new_node
             else:
                 self.__insert_recursively(current_node.right, new_node)
-   
+
+    def __delete_recursively(self, current_node, course_id):
+        if current_node is None:
+            return current_node
+        
+        if course_id < current_node.data[0]:
+            current_node.left = self.__delete_recursively(current_node.left, course_id)
+        elif course_id > current_node.data[0]:
+            current_node.right = self.__delete_recursively(current_node.right, course_id)
+        else:
+            if current_node.left is None:
+                return current_node.right
+            elif current_node.right is None:
+                return current_node.left
+            
+            min_larger_node = self.__find_min(current_node.right)
+            current_node.data = min_larger_node.data
+            current_node.right = self.__delete_recursively(current_node.right, min_larger_node.data[0])
+        
+        return current_node
+    
+    def __find_min(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+    
+    def find(self, course_id):
+        return self.__find_recursively(self.root, course_id)
+    
+    def __find_recursively(self, current_node, course_id):
+        if current_node is None:
+            return None
+        
+        if course_id == current_node.data[0]:
+            return current_node.data
+        elif course_id < current_node.data[0]:
+            return self.__find_recursively(current_node.left, course_id)
+        else:
+            return self.__find_recursively(current_node.right, course_id)
+    
